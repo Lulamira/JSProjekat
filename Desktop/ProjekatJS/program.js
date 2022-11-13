c14124ddec43229b57a3e4a9bea07e07b8b86d9d
Lulamira <samersuljic12@gmail.com>
2022-11-13 23:38:45 +0100

    IdeNovaPromjena

--- Desktop/ProjekatJS/program.js
+++ Desktop/ProjekatJS/program.js
@@ -2,4 +2,6 @@ console.log("Zdravo Samere");
 console.log("Zdravo Drugi put Samere"); 
 console.log("Zdravo Svijete jos jednom");
 console.log("Daj ba mozes li sad sta uradit");
+console.log("Hajde sad da vidimo");
+
 
